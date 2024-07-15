710d4cc8901046327eda59d8244c60f507c5d5c4
IDSILA86 <idsila87@gmail.com>
2024-07-15 15:01:29 +0400

    update

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const fileUploader = require('express-fileupload');
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'/ejs'));
+app.use(express.static('public'));
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.send('Hello world!!!');
+  res.render('index');
 });
 
 app.listen(process.env.PORT, (err) => {
