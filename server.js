0eebc3de8e8a8f2dcbda19f8d247d4723cb525a1
IDSILA86 <idsila87@gmail.com>
2024-07-15 18:47:02 +0400

    update

--- server.js
+++ server.js
@@ -8,11 +8,21 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'/ejs'));
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(fileUploader());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/upload', (req, res) => {
+  res.json('Succsefyll upload');
+});
+app.post('/upload', (req, res) => {
+  const serverFile = req.files.file;
+  serverFile.mv(__dirname + '/upload/' + serverFile.name);
+});
+
+
+
 
 app.listen(process.env.PORT, (err) => {
   err ? err : console.log('SERVER STARTED:', process.env.PORT)
